aed2215008a8151b7bc8d0fc421563d3fb163108
Yoshiya Hinosawa <stibium121@gmail.com>
2016-04-23 17:13:49 +0900

    Make test es5

--- test/test.js
+++ test/test.js
@@ -1,18 +1,18 @@
-const test = require('tape')
-const browserify = require('../')
-const path = require('path')
-const through2 = require('through2')
-const uglify = require('gulp-uglify')
-const sourcemaps = require('gulp-sourcemaps')
+var test = require('tape')
+var browserify = require('../')
+var path = require('path')
+var through2 = require('through2')
+var uglify = require('gulp-uglify')
+var sourcemaps = require('gulp-sourcemaps')
 
-const fixtureRoot = path.join(__dirname, 'fixture')
+var fixtureRoot = path.join(__dirname, 'fixture')
 
-test('browserify.src() creates vinyl stream of bundled scripts', t => {
+test('browserify.src() creates vinyl stream of bundled scripts', function (t) {
 
-  browserify.src(`${ fixtureRoot }/foo.js`).pipe(through2.obj((file, _, callback) => {
+  browserify.src(fixtureRoot + '/foo.js').pipe(through2.obj(function (file, _, callback) {
     t.ok(file.isBuffer(), 'The file is buffer')
 
-    const bundledScript = file.contents.toString()
+    var bundledScript = file.contents.toString()
 
     t.ok(/This is foo\.js/.test(bundledScript), 'The file contains foo.js')
     t.ok(/This is bar\/baz\.js/.test(bundledScript), 'The file contains bar/baz.js')
@@ -22,19 +22,19 @@ test('browserify.src() creates vinyl stream of bundled scripts', t => {
 
 })
 
-test('browserify.src() emits error when unable to bundle script', t => {
-  browserify.src(`${ fixtureRoot}/error.js`).on('error', err => {
+test('browserify.src() emits error when unable to bundle script', function (t) {
+  browserify.src(fixtureRoot + '/error.js').on('error', function (err) {
     t.ok(err instanceof Error, 'It emits error instance')
     t.end()
   })
 })
 
-test('works with uglify', t => {
+test('works with uglify', function (t) {
 
-  browserify.src(`${ fixtureRoot}/foo.js`).pipe(uglify()).pipe(through2.obj((file, enc, callback) => {
+  browserify.src(fixtureRoot + '/foo.js').pipe(uglify()).pipe(through2.obj(function (file, enc, callback) {
     t.ok(file.isBuffer(), 'The file is buffer type')
 
-    const contents = file.contents.toString()
+    var contents = file.contents.toString()
 
     t.ok(/This is foo\.js/.test(contents), 'The file contains foo.js')
     t.ok(/This is bar\/baz\.js/.test(contents), 'The file contains bar/baz/js')
@@ -44,18 +44,18 @@ test('works with uglify', t => {
 
 })
 
-test('works with sourcemaps', t => {
+test('works with sourcemaps', function (t) {
 
   browserify
-    .src(`${ fixtureRoot}/foo.js`, {debug: true})
+    .src(fixtureRoot + '/foo.js', {debug: true})
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(uglify())
     .pipe(sourcemaps.write())
-    .pipe(through2.obj((file, enc, callback) => {
+    .pipe(through2.obj(function (file, enc, callback) {
 
       t.ok(file.isBuffer(), 'The file is buffer type')
 
-      const contents = file.contents.toString()
+      var contents = file.contents.toString()
 
       t.ok(/This is foo\.js/.test(contents), 'The file contains foo.js')
       t.ok(/This is bar\/baz\.js/.test(contents), 'The file contains bar/baz/js')
